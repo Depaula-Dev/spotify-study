980dd5e9efb3a6dd68126bf72640d7a556a7ab41
Depaula-Dev <roseli.dpaula@gmail.com>
2024-02-12 16:47:05 -0300

    refactor: change main cards to script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+const apiUrl = "http://localhost:3000/main-cards";
+
+// chame o onInit assim que o carregamento da tela completar
+document.addEventListener("DOMContentLoaded", function () {
+  onInit();
+});
+
+// carregamento completo
+function onInit() {
+  getAllArtists();
+}
+
+// pegue o elemento do html com id section E faça uma chamada assicrona chamando o arquivo json ou fake back-end
+async function getAllArtists() {
+  const cardsSection = document.getElementById("section");
+  //chamada concluída
+  const data = await serviceArtist();
+
+  let colorNumber = 1;
+
+  //mostre cada card de data
+  data.forEach((element) => {
+    let link = document.createElement("a");
+
+    let card = document.createElement("div");
+    card.classList.add("cards");
+
+    card.classList.add(`card${colorNumber}`);
+
+    let title = document.createElement('h2');
+    title.textContent = element.card;
+
+    let image = document.createElement('img');
+    image.src = element.urlImg;
+
+    link.append(card);
+    card.append(title);
+    card.append(image);
+    cardsSection.append(link);
+    
+    colorNumber++;
+    console.log(card);
+  });
+}
+
+async function serviceArtist() {
+  try {
+    // aguarde a resposta do fake back-end
+    const response = await fetch(apiUrl);
+
+    // responder com os dados concluídos
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error("Erro ao obter dados");
+  }
+}
